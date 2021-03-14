37ede72656949cc41dc33dc51c2b74fe8d2283b2
Dania99dev <dania99.dev@gmail.com>
2021-03-14 16:31:32 +0330

    base functionality (grabbing marker doesn't work)

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -1,6 +1,13 @@
 <template>
+  <div id="selected-color" :style="selectedColor"></div>
   <div class="container">
-    <svg id="color-box" width="300" height="200">
+    <svg
+      id="color-box"
+      width="300"
+      height="200"
+      :style="{ backgroundColor: `hsl(${hue}, 100%, 50%)` }"
+      @mousedown="clickPos"
+    >
       <defs>
         <linearGradient id="saturation" x1="0%" y1="0%" x2="100%" y2="0%">
           <stop
@@ -19,31 +26,107 @@
       </defs>
       <rect width="100%" height="100%" fill="url(#saturation)" />
       <rect width="100%" height="100%" fill="url(#brightness)" />
+      <circle
+        id="marker"
+        cx="50%"
+        cy="50%"
+        r="6"
+        stroke="white"
+        stroke-width="2"
+        fill="transparent"
+      />
     </svg>
-    <input type="range" name="hue" id="hue" />
+    <input type="range" name="hue" id="hue" v-model="hue" min="0" max="360" />
     <p>Color picker</p>
+    hue: {{ hue }}
+    <br />
+    saturation: {{ saturation }}
+    <br />
+    brightness: {{ brightness }}
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 export default defineComponent({
-  name: "ColorPicker"
+  name: "ColorPicker",
+  data() {
+    return {
+      hue: 180,
+      saturation: 50,
+      brightness: 25
+    };
+  },
+  methods: {
+    clickPos(e: any) {
+      const colorBox = document.querySelector("#color-box");
+      const marker = document.querySelector("#marker");
+      if (colorBox !== null && marker !== null) {
+        const clickX = (e.layerX / colorBox.clientWidth) * 100;
+        const clickY = (e.layerY / colorBox.clientHeight) * 100;
+        marker.attributes[1].value = `${clickX}%`;
+        marker.attributes[2].value = `${clickY}%`;
+        this.saturation = Math.round(clickX);
+        this.brightness = Math.round(50 - clickY / 2);
+      }
+    }
+  },
+  computed: {
+    selectedColor: function(): object {
+      return {
+        backgroundColor: `hsl(${this.hue}, ${this.saturation}%, ${this.brightness}%)`
+      };
+    }
+  }
 });
 </script>
 
 <style scoped>
+#selected-color {
+  margin: 1rem;
+  width: 50px;
+  height: 50px;
+}
 .container {
-  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.05);
   padding: 1rem;
+  margin: 1rem;
   border-radius: 0.5rem;
   display: flex;
   flex-direction: column;
 }
-#color-box {
-  background-color: blue;
-}
 #hue {
   appearance: none;
   margin: 0.75rem 0;
+  background: linear-gradient(
+    to right,
+    hsl(0, 100%, 50%),
+    hsl(30, 100%, 50%),
+    hsl(60, 100%, 50%),
+    hsl(90, 100%, 50%),
+    hsl(120, 100%, 50%),
+    hsl(150, 100%, 50%),
+    hsl(180, 100%, 50%),
+    hsl(210, 100%, 50%),
+    hsl(240, 100%, 50%),
+    hsl(270, 100%, 50%),
+    hsl(300, 100%, 50%),
+    hsl(330, 100%, 50%),
+    hsl(360, 100%, 50%)
+  );
+  border-radius: 4rem;
+}
+#hue:focus {
+  outline: none;
+}
+#hue::-webkit-slider-thumb {
+  appearance: none;
+  width: 14px;
+  height: 14px;
+  border-radius: 50%;
+  border: 3px solid white;
+  cursor: grab;
+}
+#hue::-webkit-slider-thumb:active {
+  cursor: grabbing;
 }
 </style>
