e43f88f13ce4fe7983937381f521b9ce4db6e5d5
Dania99dev <dania99.dev@gmail.com>
2021-03-15 19:00:53 +0330

    data binding for marker and inputs

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -3,7 +3,7 @@
   <div class="container">
     <svg
       id="color-box"
-      width="300"
+      width="250"
       height="200"
       :style="{ backgroundColor: `hsl(${hue}, 100%, 50%)` }"
       @mousedown="isMouseDown = true"
@@ -24,8 +24,8 @@
       <rect width="100%" height="100%" fill="url(#brightness)" />
       <circle
         id="marker"
-        cx="50%"
-        cy="50%"
+        :cx="`${saturation}%`"
+        :cy="`${100 - brightness * 2}%`"
         r="6"
         stroke="white"
         stroke-width="2"
@@ -33,12 +33,17 @@
       />
     </svg>
     <input type="range" name="hue" id="hue" v-model="hue" min="0" max="360" />
-    <p>Color picker</p>
-    hue: {{ hue }}
-    <br />
-    saturation: {{ saturation }}
-    <br />
-    brightness: {{ brightness }}
+    <div class="color-inputs">
+      <div class="color-prop hue-number-input">
+        H<input type="number" v-model="hue" min="0" max="360" />
+      </div>
+      <div class="color-prop saturation-number-input">
+        S<input type="number" v-model="saturation" min="0" max="100" />
+      </div>
+      <div class="color-prop brightness-number-input">
+        B<input type="number" v-model="brightness" min="0" max="50" />
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -47,9 +52,9 @@ export default defineComponent({
   name: "ColorPicker",
   data() {
     return {
-      hue: 180,
-      saturation: 50,
-      brightness: 25,
+      hue: 360,
+      saturation: 100,
+      brightness: 50,
       isMouseDown: false
     };
   },
@@ -61,8 +66,6 @@ export default defineComponent({
         if (colorBox !== null && marker !== null) {
           const clickX = (e.layerX / colorBox.clientWidth) * 100;
           const clickY = (e.layerY / colorBox.clientHeight) * 100;
-          marker.attributes[1].value = `${clickX}%`;
-          marker.attributes[2].value = `${clickY}%`;
           this.saturation = Math.round(clickX);
           this.brightness = Math.round(50 - clickY / 2);
         }
@@ -127,5 +130,26 @@ export default defineComponent({
 }
 #hue::-webkit-slider-thumb:active {
   cursor: grabbing;
+}
+.color-inputs {
+  display: flex;
+  justify-content: space-between;
+}
+.color-prop {
+  display: flex;
+  align-items: center;
+}
+.color-prop input {
+  width: 3rem;
+  padding: 0.5rem;
+  margin: 0 0.25rem;
+  border: 1px solid #949494;
+  border-radius: 0.25rem;
+}
+.color-prop input:focus {
+  outline: none;
+}
+.color-prop input::-webkit-inner-spin-button {
+  appearance: none;
 }
 </style>
