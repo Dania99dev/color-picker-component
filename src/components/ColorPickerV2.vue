cedde0b0c3ea80c0d99416075ffcb6359196ab17
Dania99dev <dania99.dev@gmail.com>
2021-03-17 23:49:35 +0330

    dynamic content based on data

--- src/components/ColorPickerV2.vue
+++ src/components/ColorPickerV2.vue
@@ -1,6 +1,14 @@
+// ! line 9 should be fixed after setting conversion functions
 <template>
   <div class="container">
-    <svg id="color-box" :width="colorBoxWidth" :height="colorBoxHeight">
+    <svg
+      id="color-box"
+      :width="colorBoxWidth"
+      :height="colorBoxHeight"
+      :style="{
+        backgroundColor: `hsl(${selectedColor.hsb.h}, 100%, 50%)`
+      }"
+    >
       <defs>
         <linearGradient id="saturation" x1="0%" y1="0%" x2="100%" y2="0%">
           <stop offset="0%" style="stop-color:rgba(255,255,255,1)" />
@@ -15,8 +23,8 @@
       <rect width="100%" height="100%" fill="url(#brightness)" />
       <circle
         id="white-marker"
-        cx="50%"
-        cy="50%"
+        :cx="`${selectedColor.hsb.s}%`"
+        :cy="`${100 - selectedColor.hsb.b}%`"
         r="6"
         stroke="white"
         stroke-width="2"
@@ -24,15 +32,35 @@
       />
       <circle
         id="black-marker"
-        cx="50%"
-        cy="50%"
+        :cx="`${selectedColor.hsb.s}%`"
+        :cy="`${100 - selectedColor.hsb.b}%`"
         r="4"
         stroke="black"
         stroke-width="2"
         fill="transparent"
       />
     </svg>
-    <input type="range" id="hue" min="0" max="360" />
+    <input
+      type="range"
+      id="hue"
+      min="0"
+      max="360"
+      v-model="selectedColor.hsb.h"
+    />
+    <div class="color-inputs">
+      <div class="color-prop">
+        <p>H</p>
+        <input type="number" v-model="selectedColor.hsb.h" min="0" max="360" />
+      </div>
+      <div class="color-prop">
+        <p>S</p>
+        <input type="number" v-model="selectedColor.hsb.s" min="0" max="100" />
+      </div>
+      <div class="color-prop">
+        <p>B</p>
+        <input type="number" v-model="selectedColor.hsb.b" min="0" max="100" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -49,6 +77,32 @@ export default defineComponent({
       type: Number,
       reqired: true
     }
+  },
+  data() {
+    return {
+      selectedColor: {
+        hsb: {
+          h: 360,
+          s: 100,
+          b: 100
+        },
+        hsl: {
+          h: 360,
+          s: 100,
+          l: 100
+        },
+        rgb: {
+          r: 255,
+          g: 255,
+          b: 255
+        },
+        hex: {
+          r: "ff",
+          g: "ff",
+          b: "ff"
+        }
+      }
+    };
   }
 });
 </script>
@@ -98,5 +152,26 @@ export default defineComponent({
 }
 #hue::-webkit-slider-thumb:active {
   cursor: grabbing;
+}
+.color-inputs {
+  display: flex;
+  justify-content: space-between;
+}
+.color-prop {
+  display: flex;
+  align-items: center;
+}
+.color-prop input {
+  width: 3rem;
+  padding: 0.5rem;
+  margin: 0 0.25rem;
+  border: 1px solid #949494;
+  border-radius: 0.25rem;
+}
+.color-prop input:focus {
+  outline: none;
+}
+.color-prop input::-webkit-inner-spin-button {
+  appearance: none;
 }
 </style>
