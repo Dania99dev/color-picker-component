7c75ce271b5e340e5e1a51533e22602151b42f1a
Dania99dev <dania99.dev@gmail.com>
2021-03-19 11:07:24 +0330

    types added

---
+++ src/types.ts
@@ -0,0 +1,20 @@
+export interface HSB {
+  h: number,
+  s: number,
+  b: number
+}
+export interface HSL {
+  h: number,
+  s: number,
+  l: number
+}
+export interface RGB {
+  r: number,
+  g: number,
+  b: number
+}
+export interface HEX {
+  r: string,
+  g: string,
+  b: string
+}
